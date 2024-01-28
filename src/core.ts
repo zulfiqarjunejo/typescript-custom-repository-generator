45a7f34f65eaaa27e11eeb45f770bf0efa34e042
Zulfiqar Ahmed <zulfiqar.ahmed@e-go-mobile.com>
2024-01-28 19:42:45 +0100

    second iteration

--- src/core.ts
+++ src/core.ts
@@ -1,19 +1,6 @@
+import { capitalize } from 'lodash';
 import handlebars from './handlebars';
 import { Entity } from "./types";
-
-const codeTemplate = `
-{{#each searchEnablers}}
-export type FindBy{{capitalize this.name}}Opts = {
-    {{this.name}}: {{this.type}};
-};
-{{/each}}
-
-export interface I{{capitalize entityName}}Repository {
-    {{#each searchEnablers}}
-    findBy{{capitalize this.name}}(opts: FindBy{{capitalize this.name}}Opts): Promise<{{capitalize this.name}} | undefined>;
-    {{/each}}
-};
-`;
 
 type SearchEnabler = {
     name: string;
@@ -27,8 +14,44 @@ export const generateRepository = (opts: GenerateRepositoryOpts): any => {
     const name = opts.entity.name.trim();
     const props = opts.entity.props;
 
+    const capitalizedEntityName = capitalize(name);
+
+    const codeTemplate = `
+
+/**
+ * \`WithContext\` represents a generic type containing \`context\`. 
+ */
+export type WithContext = {
+    /**
+     * context, if available.
+     */
+    context?: IDataContext;
+};
+
+export type CreateOpts = MaybeWithContext & {
+    {{#each props}}
+    {{this.name}}: {{this.type}};
+    {{/each}}
+};
+
+{{#each searchEnablers}}
+export type FindBy{{capitalize this.name}}Opts = {
+    {{this.name}}: {{this.type}};
+};
+{{/each}}
+
+export interface I{{capitalize entityName}}Repository {
+    create(opts: CreateOpts): Promise<${capitalizedEntityName} | undefined>;
+    {{#each searchEnablers}}
+    findBy{{capitalize this.name}}(opts: FindBy{{capitalize this.name}}Opts): Promise<${capitalizedEntityName} | undefined>;
+    {{/each}}
+    list(opts: ListOpts): Promise<${capitalizedEntityName}[]>;
+    toView(opts: ToViewOpts): Promise<${capitalizedEntityName}View>;
+    update(opts: UpdateOpts): Promise<${capitalizedEntityName} | undefined>;
+};
+`;
+
     const searchEnablers: SearchEnabler[] = [];
-    
     for (const prop of props) {
         if (prop.searchEnabler) {
             const se: SearchEnabler = {
@@ -44,7 +67,8 @@ export const generateRepository = (opts: GenerateRepositoryOpts): any => {
 
     const templateData = {
         "entityName": name,
-        "searchEnablers": searchEnablers
+        "props": props,
+        "searchEnablers": searchEnablers,
     };
 
     const generatedContent = template(templateData);
